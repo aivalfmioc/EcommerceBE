65a989b37d84ca6a512070b61758ff085df74f07
aivalfmioc <flaviamioc@yahoo.com>
2023-06-07 16:56:25 +0300

    added configuration for HTTPS and other BE functionalities

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -1,24 +1,31 @@
 package com.luv2code.ecommerce.service;
 
 import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.entity.Customer;
 import com.luv2code.ecommerce.entity.Order;
 import com.luv2code.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
     private CustomerRepository customerRepository;
 
-    public CheckoutServiceImpl(CustomerRepository customerRepository){
+    public CheckoutServiceImpl(CustomerRepository customerRepository,
+                               @Value("${stripe.key.secret}")String secretKey){
         this.customerRepository= customerRepository;
+        //initialize Stripe API with the secret key
+        Stripe.apiKey = secretKey;
     }
     @Override
     @Transactional
@@ -39,11 +46,33 @@ public class CheckoutServiceImpl implements CheckoutService {
         order.setShippingAddress(purchase.getShippingAddress());
         //populate customer with order
         Customer customer = purchase.getCustomer();
+
+        // check if this is an existing customer
+        String theEmail = customer.getEmail();
+
+        Customer customerFromDB = customerRepository.findByEmail(theEmail);
+
+        if(customerFromDB != null){
+            //we found let s assign accordingly
+            customer = customerFromDB;
+        }
         customer.add(order);
         //save to the database
         customerRepository.save(customer);
         //return a response
         return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+        return PaymentIntent.create(params);
     }
 
     private String generateOrderTrackingNumber() {
